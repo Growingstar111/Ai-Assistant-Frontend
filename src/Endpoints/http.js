b0b824e2e0c08c1dec791f563938badd001fe911
Vivek Sharma <ranusharma14112003@gmail.com>
2025-10-15 02:48:31 +0530

    base url updated

--- src/Endpoints/http.js
+++ src/Endpoints/http.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "https://ai-assistant-backend-6aaq.onrender.com",
+  baseURL: "https://ai-assistant-backend-6aaq.onrender.com/api",
 });
